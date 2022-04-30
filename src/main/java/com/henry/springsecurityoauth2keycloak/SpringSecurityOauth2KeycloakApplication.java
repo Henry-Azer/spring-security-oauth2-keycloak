02131544f40c424b1060f54ef4c7b7159148e69f
Henry-Azer <henryazer@outlook.com>
2022-04-30 18:54:27 +0200

    initial commit

---
+++ src/main/java/com/henry/springsecurityoauth2keycloak/SpringSecurityOauth2KeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.henry.springsecurityoauth2keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2KeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauth2KeycloakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henry/springsecurityoauth2keycloak/SpringSecurityOauth2KeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henry.springsecurityoauth2keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2KeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
