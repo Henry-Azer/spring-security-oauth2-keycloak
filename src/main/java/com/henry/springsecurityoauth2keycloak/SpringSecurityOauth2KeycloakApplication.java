442f3f8ab8d1e8f813bd1b92200c6e7874c7cc93
Henry-Azer <henryazer@outlook.com>
2022-04-30 20:13:18 +0200

    update sprint boot apps

--- src/main/java/com/henry/springsecurityoauth2keycloak/SpringSecurityOauth2KeycloakApplication.java
+++ src/main/java/com/henry/springsecurityoauth2keycloak/SpringSecurityOauth2KeycloakApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringSecurityOauth2KeycloakApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityOauth2KeycloakApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOauth2KeycloakApplication.class, args);
+    }
 
 }
--- src/test/java/com/henry/springsecurityoauth2keycloak/SpringSecurityOauth2KeycloakApplicationTests.java
+++ src/test/java/com/henry/springsecurityoauth2keycloak/SpringSecurityOauth2KeycloakApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringSecurityOauth2KeycloakApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
